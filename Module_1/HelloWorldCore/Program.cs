f18b145bbec1d6ac5258fd5a519c623f87883f46
YauheniyaLiahovich <Jan_cdth_26@mail.ru>
2020-10-11 20:14:34 +0300

    add module 1

---
+++ Module_1/HelloWorldCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorldCore
+{
+    /// <summary>
+    /// HelloWorld
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            //add text to console
+            //TODO refactor me
+            Console.WriteLine("Hello World!");
+
+            var todayDate = GetTodayDate();
+            Console.WriteLine(todayDate);
+        }
+
+        public static DateTime GetTodayDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ Module_1/HelloWorldTest/HelloWorldTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class HelloWorldTests
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorldCore.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
+
+
